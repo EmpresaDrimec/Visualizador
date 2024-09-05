50340d5b3a3774be915894e4643a2a6745bcd8bc
EmpresaDrimec <learningmachine621@gmail.com>
2024-09-04 20:56:29 -0400

    1

---
+++ Js/iot-mqtt.js
@@ -0,0 +1,29 @@
+var clientId = "ws" + Math.random();
+// Create a client instance
+var client = new Paho.MQTT.Client("192.168.0.3", 9001, clientId);
+
+// set callback handlers
+client.onConnectionLost = onConnectionLost;
+client.onMessageArrived = onMessageArrived;
+
+// connect the client
+client.connect({onSuccess:onConnect});
+
+
+// called when the client connects
+function onConnect() {
+  // Once a connection has been made, make a subscription and send a message.
+  console.log("Conectado MQTT-WebSocket");
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    console.log("onConnectionLost:"+responseObject.errorMessage);
+  }
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+  console.log("onMessageArrived:"+message.payloadString);
+}
